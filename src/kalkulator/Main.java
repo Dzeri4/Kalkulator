4419d1c343d42743554e1f16b51481b5b8881dab
Vojin Dzeletovic <vojin.00@gmail.com>
2021-11-10 23:13:48 +0100

    initial read loop

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,10 +1,24 @@
 package kalkulator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner skener;
+		String s;
+		
+		skener = new Scanner(System.in);
+		
+		
+		do {
+			
+			s = skener.nextLine();
+			
+		} while(!s.equals("kraj"));
 
+		
 	}
-
 }
